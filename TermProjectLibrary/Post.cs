b71f60a2b8c5242d38fbd224f2d082ca9018f163
Frank Gatto <frankagatto98@gmail.com>
2022-12-01 15:46:31 -0500

    added classes

--- TermProjectLibrary/Post.cs
+++ TermProjectLibrary/Post.cs
@@ -6,5 +6,16 @@ namespace TermProjectLibrary
 {
     class Post
     {
+        private int id;
+        private int likes;
+        private int dislikes;
+        private string username;
+        private string content;
+
+        public int Id { get => id; set => id = value; }
+        public int Likes { get => likes; set => likes = value; }
+        public int Dislikes { get => dislikes; set => dislikes = value; }
+        public string Username { get => username; set => username = value; }
+        public string Content { get => content; set => content = value; }
     }
 }
--- TermProjectLibrary/UserAccount.cs
+++ TermProjectLibrary/UserAccount.cs
@@ -4,5 +4,30 @@ namespace TermProjectLibrary
 {
     public class UserAccount
     {
+        private string username;
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string homeAddress;
+        private string billingAddress;
+        private string phoneNumber;
+        private string profileImageKey;
+        private string isEmailConfirmed;
+        private string securityQuestion1Answer;
+        private string securityQuestion2Answer;
+        private string securityQuestion3Answer;
+
+        public string Username { get => username; set => username = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Email { get => email; set => email = value; }
+        public string HomeAddress { get => homeAddress; set => homeAddress = value; }
+        public string BillingAddress { get => billingAddress; set => billingAddress = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string ProfileImageKey { get => profileImageKey; set => profileImageKey = value; }
+        public string IsEmailConfirmed { get => isEmailConfirmed; set => isEmailConfirmed = value; }
+        public string SecurityQuestion1Answer { get => securityQuestion1Answer; set => securityQuestion1Answer = value; }
+        public string SecurityQuestion2Answer { get => securityQuestion2Answer; set => securityQuestion2Answer = value; }
+        public string SecurityQuestion3Answer { get => securityQuestion3Answer; set => securityQuestion3Answer = value; }
     }
 }
