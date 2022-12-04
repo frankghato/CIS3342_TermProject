7fda6417d28856123b925116af6c0a4d81ad841f
JablonskiJacob <63124095+JablonskiJacob@users.noreply.github.com>
2022-12-03 20:53:33 -0500

    Added Web API

---
+++ TermProjectAPI/Controllers/PostController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using TermProjectLibrary;
+using Utilities;
+
+namespace TermProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class PostController : Controller
+    {
+        [HttpGet]
+        public List<Post> GetPosts() // Get All posts
+        {
+            DBConnect objDB = new DBConnect(); // SQL Objects needed for calls
+            SqlCommand objCommand = new SqlCommand();
+
+            objCommand.CommandType = CommandType.StoredProcedure; // Set type to procedure
+            objCommand.CommandText = "TP_GetPosts";
+
+            DataSet myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+            List<Post> Posts = new List<Post>();
+            Post p;
+
+            foreach (DataRow record in myDS.Tables[0].Rows)
+            {
+                p = new Post();
+                p.Id = int.Parse(record["PostID"].ToString());
+                p.Username = record["Username"].ToString();
+                p.Content = record["Content"].ToString();
+                p.Likes = int.Parse(record["Likes"].ToString());
+                p.Dislikes = int.Parse(record["Dislikes"].ToString());
+                Posts.Add(p);
+            }
+            return Posts;
+        }
+
+        [HttpGet("{username}")]
+        public List<Post> GetPostsByUser(string username)
+        {
+            DBConnect objDB = new DBConnect(); // SQL Objects needed for calls
+            SqlCommand objCommand = new SqlCommand();
+
+            objCommand.CommandType = CommandType.StoredProcedure; // Set type to procedure
+            objCommand.CommandText = "TP_GetPostsFromUsername";
+            objCommand.Parameters.AddWithValue("@theUsername", username);
+
+            DataSet myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+            List<Post> Posts = new List<Post>();
+            Post p;
+
+            foreach (DataRow record in myDS.Tables[0].Rows)
+            {
+                p = new Post();
+                p.Id = int.Parse(record["PostID"].ToString());
+                p.Username = record["Username"].ToString();
+                p.Content = record["Content"].ToString();
+                p.Likes = int.Parse(record["Likes"].ToString());
+                p.Dislikes = int.Parse(record["Dislikes"].ToString());
+                Posts.Add(p);
+            }
+            return Posts;
+        }
+    }
+}
---
+++ TermProjectAPI/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TermProjectLibrary
+{
+    public class Post
+    {
+        private int id;
+        private int likes;
+        private int dislikes;
+        private string username;
+        private string content;
+
+        public int Id { get => id; set => id = value; }
+        public int Likes { get => likes; set => likes = value; }
+        public int Dislikes { get => dislikes; set => dislikes = value; }
+        public string Username { get => username; set => username = value; }
+        public string Content { get => content; set => content = value; }
+    }
+}
