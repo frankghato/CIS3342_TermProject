d4935ec876877297fe6af53032df30fff4446be4
frankghato <59925619+frankghato@users.noreply.github.com>
2022-12-05 16:21:05 -0500

    added button to delete cookie

--- CIS3342_TermProject/Login.aspx.designer.cs
+++ CIS3342_TermProject/Login.aspx.designer.cs
@@ -166,5 +166,14 @@ namespace CIS3342_TermProject
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl loginInformation;
+
+        /// <summary>
+        /// btnClearCookie control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnClearCookie;
     }
 }
