30ab0ae26e001cf489bd8c7c56235dd8a96152af
frankghato <59925619+frankghato@users.noreply.github.com>
2022-12-04 20:02:04 -0500

    added api functions and calls to confirm email address and check if email address is confirmed

--- CIS3342_TermProject/EmailConfirmation.aspx.cs
+++ CIS3342_TermProject/EmailConfirmation.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -20,6 +23,34 @@ namespace CIS3342_TermProject
             else
             {
                 welcomeMessage.InnerText = email + ", thank you for confirming your account.";
+
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                String jsonPost = js.Serialize(p);
+                try
+                {
+                    WebRequest request = WebRequest.Create("https://localhost:44382/api/user/ConfirmEmail");
+                    request.Method = "PUT";
+                    request.ContentLength = jsonPost.Length;
+                    request.ContentType = "application/json";
+
+                    StreamWriter writer = new StreamWriter(request.GetRequestStream());
+                    writer.Write(jsonPost);
+                    writer.Flush();
+                    writer.Close();
+
+                    WebResponse response = request.GetResponse();
+                    Stream dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    String data = reader.ReadToEnd();
+                    reader.Close();
+                    response.Close();
+                }
+
+                catch (Exception ex)
+                {
+                    welcomeMessage.InnerText = "Error: " + ex.Message;
+                }
+
             }
         }
     }
--- TermProjectAPI/Controllers/UserController.cs
+++ TermProjectAPI/Controllers/UserController.cs
@@ -39,5 +39,43 @@ namespace TermProjectAPI.Controllers
             }
             return password;
         }
+
+        [HttpGet("GetIsEmailConfirmed/{username}")] // Get isemailconfirmed field of username
+        public string GetIsEmailConfirmed(string username)
+        {
+            DBConnect objDB = new DBConnect(); // SQL Objects needed for calls
+            SqlCommand objCommand = new SqlCommand();
+
+            objCommand.CommandType = CommandType.StoredProcedure; // Set type to procedure
+            objCommand.CommandText = "TP_GetIsEmailConfirmed";
+            objCommand.Parameters.AddWithValue("@theUsername", username);
+
+            DataSet myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+            string isEmailConfirmed = "";
+            if (myDS.Tables[0].Rows.Count != 0)
+            {
+                DataRow record = myDS.Tables[0].Rows[0];
+                isEmailConfirmed = record["IsEmailConfirmed"].ToString();
+            }
+            return isEmailConfirmed;
+        }
+
+        [HttpPut("ConfirmEmail")] // confirm email of username
+        public Boolean ConfirmEmail(string username)
+        {
+            DBConnect objDB = new DBConnect(); // SQL Objects needed for calls
+            SqlCommand objCommand = new SqlCommand();
+
+            objCommand.CommandType = CommandType.StoredProcedure; // Set type to procedure
+            objCommand.CommandText = "TP_ConfirmEmail";
+            objCommand.Parameters.AddWithValue("@theUsername", username);
+
+            int updated = objDB.DoUpdateUsingCmdObj(objCommand);
+            if (updated > 0)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
